a815c4c5220408b4b1eb2caf0ac961fb9eea7341
Aleksandr-hub-cyber <141833331+Aleksandr-hub-cyber@users.noreply.github.com>
2024-04-30 16:41:35 +0800

    Спринт №2: Добавление новых методов для REST API

--- Location/position/views.py
+++ Location/position/views.py
@@ -21,3 +21,45 @@ def submit_data(request):
         except IntegrityError:
             return HttpResponse(status=400)
     return HttpResponse(status=405)
+
+@csrf_exempt
+def get_data(request, id):
+    if request.method == 'GET':
+        try:
+            pass = Pass.objects.get(id=id)
+            return HttpResponse(status=200, content=pass)
+        except Pass.DoesNotExist:
+            return HttpResponse(status=404)
+    return HttpResponse(status=405)
+
+@csrf_exempt
+def update_data(request, id):
+    if request.method == 'PATCH':
+        try:
+            pass = Pass.objects.get(id=id)
+            if pass.status == 'new':
+                data = request.json
+                pass.coordinates = data['coordinates']
+                pass.elevation = data['elevation']
+                pass.name = data['name']
+                pass.photos = data['photos']
+                pass.save()
+                return HttpResponse(status=200, content={'state': 1, 'message': 'Successfully updated'})
+            else:
+                return HttpResponse(status=403, content={'state': 0, 'message': 'Cannot update data'})
+        except Pass.DoesNotExist:
+            return HttpResponse(status=404)
+        except ValidationError as e:
+            return HttpResponse(status=400, content={'state': 0, 'message': str(e)})
+    return HttpResponse(status=405)
+
+@csrf_exempt
+def get_data_by_user(request):
+    if request.method == 'GET':
+        email = request.GET.get('user__email')
+        if email:
+            passes = Pass.objects.filter(user_email=email)
+            return HttpResponse(status=200, content=passes)
+        else:
+            return HttpResponse(status=400)
+    return HttpResponse(status=405)
